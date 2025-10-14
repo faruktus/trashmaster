de7a7a7be3733d8977e58938f6f17ebcc2662a18
faruktus <klem2@gmx.net>
2025-10-14 16:19:19 +0200

    working algorithm for data input

--- putz.py
+++ putz.py
@@ -6,59 +6,81 @@ import time
 import json
 
 Bereiche = ["Vorraum", "Garderobe", "WC"]
-dicti = {i+1: x for i, x in enumerate(sorted(Bereiche))}
+dict_Bereiche = {i+1: x for i, x in enumerate(sorted(Bereiche))}
 
 year = 2025
 month = 3
 day = 1
 
+dict_interval={}
+Putzintervall = {1: "Woche", 2: "Monat", 3: "Jahr"}
+
+
+while True:
+    print(dict_Bereiche)
+    auswahl_Bereich = input("Choose: ")
+    try:
+        var = int(auswahl_Bereich)
+        auswahl_Bereich = dict_Bereiche[var]
+    except ValueError:
+        print("no Integer")
+        continue
+    except KeyError:
+        print("not in Keys")
+        continue
+
+    print(Putzintervall)
+    eingabe_Zeitrahmen = input(auswahl_Bereich + ": ")
+    try:
+        var = int(eingabe_Zeitrahmen)
+        eingabe_Zeitrahmen = Putzintervall[var]
+    except ValueError:
+        print("no Integer")
+        continue
+    except KeyError:
+        print("not in Keys")
+        continue
+
+    print()
+    eingabe_Intervall = input("Wie oft pro " + eingabe_Zeitrahmen + " soll geputzt werden: ")
+    try:
+        eingabe_Intervall = int(eingabe_Intervall)
+        break
+    except:
+        print("no Integer")
+
+    
+dict_interval[auswahl_Bereich]= [eingabe_Zeitrahmen, eingabe_Intervall]
+print(dict_interval)
+        
+
+#dict_interval = {'Garderobe': ['Woche', 1], 'Vorraum': ['Monat', 1], 'WC': ['Jahr', 3]}
+print(dict_interval)
+
 starting_date=datetime(year, month, day)
 plusyear = starting_date + relativedelta(years=1)
 
-
-dict_inter={}
-eingabe = ""
-while True:
-    print(dicti)
-    auswahl_Bereich = input("Choose: ")
-    if auswahl_Bereich == "aus":
-        break
-
-    if int(auswahl_Bereich) not in dicti.keys():
-        print("no")
-        continue
-    
-    else:
-        auswahl_Bereich = int(auswahl_Bereich)
-        eingabe = input("Woche, Monat, Jahr: ")
-        if eingabe == "aus":
-            break
-
-        inter = input("Intervall: ")
-        if inter == "aus":
-            break
-        else:
-            inter = int(inter)
-
-        if eingabe == "Woche":
-            calc = round(365/53/inter)
-        if eingabe == "Monat":
-            calc = round(365/12/inter)
-        if eingabe == "Jahr":
-            calc = round(365/inter)
+dict_days={}
+global calc
+for k,v in dict_interval.items():
+        if v[0] == "Woche":
+            calc = round(365/53/v[1])
+        if v[0]  == "Monat":
+            calc = round(365/12/v[1])
+        if v[0]  == "Jahr":
+            calc = round(365/v[1])
 
         listi=[]
         new_start = starting_date
         while starting_date < plusyear:
             starting_date += relativedelta(days=calc)
             listi.append(starting_date.strftime("%d, %m, %Y"))
-        dict_inter[dicti[auswahl_Bereich]] = listi
+        dict_days[k] = listi
         starting_date=new_start
 
-print(dict_inter)
+print(dict_days)
 
 """
-
 with open("deimuada.json", mode="w", encoding="utf-8") as write_file:
     json.dump(dicti, write_file, indent=2)
         
