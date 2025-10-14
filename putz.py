d0dd9e3c6e2c2efae41bfcd928e40ab1f477176f
faruktus <klem2@gmx.net>
2025-10-14 16:08:48 +0200

    first json creation

--- putz.py
+++ putz.py
@@ -1,44 +1,75 @@
 import locale, datetime
 locale.setlocale(locale.LC_ALL, 'de_DE.utf8')
-from datetime import date, datetime
+from datetime import date, datetime 
 from dateutil.relativedelta import relativedelta
-import calendar
+import time
+import json
+
+Bereiche = ["Vorraum", "Garderobe", "WC"]
+
+year = 2025
+month = 3
+day = 1
+
+starting_date=datetime(year, month, day)
+plusyear = starting_date + relativedelta(years=1)
 
 
+dicti={}
+for x in Bereiche:
+    eingabe = input(x + "Woche, Monat, Jahr: ")
+    inter = int(input("Intervall: "))
+
+    if eingabe == "Woche":
+        calc = round(365/53/inter)
+    if eingabe == "Monat":
+        calc = round(365/12/inter)
+    if eingabe == "Jahr":
+        calc = round(365/inter)
+
+    listi=[]
+    new_start = starting_date
+    while starting_date < plusyear:
+        starting_date += relativedelta(days=calc)
+        listi.append(starting_date.strftime("%d, %m, %Y"))
+    dicti[x] = listi
+    starting_date=new_start
+
+with open("deimuada.json", mode="w", encoding="utf-8") as write_file:
+    json.dump(dicti, write_file, indent=2)
+
+        
+
+    
+
+
+
+
+
+
+
+
+
+
+"""
+def find_first(year, month):
+    d = datetime(year, int(month), 7)
+    offset = -d.weekday()
+    print(offset)
+    return d + timedelta(offset)
+
+print(find_first(2025, 1))
 heute = date.today()
-
 curr_month=datetime.now().month
 curr_year=datetime.now().year
-curr_day=datetime.now().day
+curr_week=datetime.now().isocalendar()[1]
+print(first_Monday.isocalender[1])
 #days_curr_month=calendar.monthrange(2025, curr_month)[1]
-days_curr_month=7
 
-inter=3
-
-if inter > 1:
-    div = round(days_curr_month/(inter))
-    listi=[]
-else:
-    listi=[15]
-    inter=0
-
-cnt=1
-for x in range(inter):
-    listi.append(cnt)
-    cnt+=div-1
-
-print(listi)
-
-listi=[datetime(curr_year, curr_month, x).strftime("%d, %m, %Y") for x in sorted(listi)]
-print(listi)
-
-
-
-
-
-
-
-
+#print(hola.strftime("%d, %m, %Y, %A"))
+cnt+=timedelta(days=div)
+listi=[x.strftime("%d, %m, %Y") for x in sorted(listi)]
+"""
     
 
 
@@ -53,6 +84,3 @@ print(listi)
 
 #print(deimuada.days)
 
-delta=relativedelta(months=+1)
-
-Geburtstag = date(2025, 3, 28)
